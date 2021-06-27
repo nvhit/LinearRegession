dc284e99a01ba76cc87375351f04655f4f6ba207
nvhit <vanhungnguyen.vinhuni@gmail.com>
2021-06-27 18:32:01 +0700

    add model

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 from argparse import ArgumentParser
 import numpy as np
 import tensorflow as tf
@@ -7,6 +8,36 @@ import tempfile
 import requests
 import numpy as np
 import tensorflow as tf
+
+
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
+    """
+    Freezes the state of a session into a pruned computation graph.
+
+    Creates a new computation graph where variable nodes are replaced by
+    constants taking their current value in the session. The new graph will be
+    pruned so subgraphs that are not necessary to compute the requested
+    outputs are removed.
+    @param session The TensorFlow session to be frozen.
+    @param keep_var_names A list of variable names that should not be frozen,
+                          or None to freeze all the variables in the graph.
+    @param output_names Names of the relevant graph outputs.
+    @param clear_devices Remove the device directives from the graph for better portability.
+    @return The frozen graph definition.
+    """
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ''
+        frozen_graph = tf.graph_util.convert_variables_to_constants(
+            session, input_graph_def, output_names, freeze_var_names)
+        return frozen_graph
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -38,12 +69,14 @@ if __name__ == "__main__":
 
     print(model.predict([12.0]))
 
+    file_pb = "./output/out_linear_pb"
+    file_h5 = "./output/out_linear.h5"
 
+    model.save(file_h5)
+
+    tf.saved_model.save(model, file_pb)
 
     xs = np.array([[9.0], [50.0], [2.0], [3.0]])
     data = json.dumps({"signature_name": "serving_default", "instances": xs.tolist()})
     print(data)
 
-headers = {"content-type": "application/json"}
-json_response = requests.post('http://localhost:8501/v1/models/helloworld:predict', data=data, headers=headers)
-print(json_response.text)
